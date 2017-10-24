32477f1c158aae84f3226347e126f724f78fcc2d
Matt Gibson <matt@doormatt.com>
2017-10-23 20:09:55 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='epsolar-tracer',
+    version='',
+    packages=['test', 'pyepsolartracer'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
